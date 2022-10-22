dd99b7e254d93ba855693779a5ca64b6b35b8dbc
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-10-22 18:43:08 -0300

    pattern directory structure
    organize the structure

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
 import uvicorn
 
+from routers.items import router as router_item
+
+# Instãncia da API
 app = FastAPI(title="MyAPI",
               description="Welcome to my test API, enjoy!",
               contact={
@@ -10,30 +12,8 @@ app = FastAPI(title="MyAPI",
               },
               )
 
-lista = []
-
-
-@app.get("/", tags=["Welcome"])
-async def root():
-    return {"message": "Welcome to my test API!"}
-
-
-@app.post("/item/{name}", tags=["Items"])
-async def add_item(name: str):
-    lista.append(name)
-    return {"message": "The item was successfully added!"}
-
-
-@app.get("/allitems", tags=["Items"])
-async def all_items():
-    return {"message": jsonable_encoder(lista)}
-
-
-@app.delete("/item/{name}", tags=["Items"])
-async def delete_item(name: str):
-    lista.remove(name)
-    return {"message": "The item was successfully removed!"}
-
+# Adiciona a rota de item na api
+app.include_router(router_item)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080)
---
+++ routers/items.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.encoders import jsonable_encoder
+
+# instância da rota de itens
+router = APIRouter(
+    prefix="/items",
+    tags=["items"],
+    responses={404: {"description": "Not found"}},
+)
+
+# Dados mocados
+items = ["test1", "test2", "test3"]
+
+
+# Ler todos os itens
+@router.get("/")
+async def read_items():
+    return {"message": jsonable_encoder(items)}
+
+
+# Ler um item pelo id
+@router.get("/{item_id}")
+async def read_item(item_id: int):
+    try:
+        return {"message": items[item_id]}
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+# Adicionar um item
+@router.post("/{name}")
+async def add_item(name: str):
+    items.append(name)
+    return {"message": "The item was successfully added!"}
+
+
+# Deletar item pelo id
+@router.delete("/{item_id}")
+async def delete_item(item_id: int):
+    try:
+        del items[item_id]
+        return {"message": "The item was successfully removed!"}
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Item not found")
